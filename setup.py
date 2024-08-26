836f73dd042372d947eae83a928a00a026416706
simben <simben@kth.se>
2024-08-26 14:21:49 +0200

    Scripts updated

--- setup.py
+++ setup.py
@@ -4,6 +4,17 @@ from setuptools import setup
 import versioneer
 
 
+def resolve_requirements(file):
+    requirements = []
+    with open(file) as f:
+        req = f.read().splitlines()
+        for r in req:
+            if r.startswith("-r"):
+                requirements += resolve_requirements(os.path.join(os.path.dirname(file), r.split(" ")[1]))
+            else:
+                requirements.append(r)
+    return requirements
+
 def read_file(file):
     with open(file) as f:
         content = f.read()
@@ -17,12 +28,14 @@ setup(
     zip_safe=False,
     data_files=[('', ["requirements.txt"]), ],
     package_data={
-        "": ["configs/*.yml", "configs/*.json"],
+        "": ["configs/*.json"],
     },
     entry_points={
         "console_scripts": [
             "MAIAKubeGate_deploy_helm_chart = MAIAKubeGate_scripts.MAIAKubeGate_deploy_helm_chart:main",
-            "MAIAKubeGate_build_and_push_docker_image = MAIAKubeGate_scripts.MAIAKubeGate_build_and_push_docker_image:main"
+            "MAIAKubeGate_deploy_MAIA_namespace = MAIAKubeGate_scripts.MAIAKubeGate_deploy_MAIA_namespace:main",
+            "MAIAKubeGate_create_JupyterHub_config = MAIAKubeGate_scripts.MAIAKubeGate_create_JupyterHub_config:main",
+            "MAIAKubeGate_create_MAIA_Addons_config =  MAIAKubeGate_scripts.MAIAKubeGate_create_MAIA_Addons_config:main",
         ],
     },
     keywords=["helm", "kubernetes", "maia", "resource deployment"],
