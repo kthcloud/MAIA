d704f588ac0da68c9eca876d1c888c10b536c459
simben <simben@kth.se>
2025-01-26 20:36:20 +0100

    Update maia-toolkit installation to use the latest version

--- dashboard/start.sh
+++ dashboard/start.sh
@@ -2,7 +2,7 @@
 
 helm repo add maia https://kthcloud.github.io/MAIA/
 helm repo update
-python -m pip install maia-toolkit==1.6.4
+python -m pip install maia-toolkit
 
 python manage.py makemigrations authentication
 python manage.py makemigrations
