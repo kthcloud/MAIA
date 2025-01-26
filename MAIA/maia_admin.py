0d1972c063578d8ae4a2ce642c7c09e25dbc9781
simben <simben@kth.se>
2025-01-26 21:15:15 +0100

    Disable debug mode in create_maia_dashboard_values function

--- MAIA/maia_admin.py
+++ MAIA/maia_admin.py
@@ -889,7 +889,7 @@ def create_maia_dashboard_values(config_folder, project_id, cluster_config_dict,
     if "discord_signup_url" in maia_config_dict:
         maia_dashboard_values["dashboard"]["discord_signup_url"] = maia_config_dict["discord_signup_url"]
     
-    debug = True
+    debug = False
     
     if debug:
     
