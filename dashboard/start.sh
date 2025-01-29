de98bb92c6b73e833ae6a209a6f5eea65dc78239
simben <simben@kth.se>
2025-01-29 23:16:54 +0100

    Update start.sh to install MAIA from local directory instead of using editable mode

--- dashboard/start.sh
+++ dashboard/start.sh
@@ -4,7 +4,7 @@ helm repo add maia https://kthcloud.github.io/MAIA/
 helm repo update
 #python -m pip install maia-toolkit
 git clone https://github.com/kthcloud/MAIA.git
-pip install -e MAIA
+pip install ./MAIA
 python manage.py makemigrations authentication
 python manage.py makemigrations
 python manage.py migrate
