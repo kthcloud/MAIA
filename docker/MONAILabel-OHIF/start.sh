7e3a538579b852f05f96942a22411777e91368bb
simben <simben@kth.se>
2025-01-30 16:25:09 +0100

    Refactor start.sh to conditionally install dependencies and improve model handling logic

--- docker/MONAILabel-OHIF/start.sh
+++ docker/MONAILabel-OHIF/start.sh
@@ -15,28 +15,28 @@ sed -i "s+INGRESS_PATH+$MONAI_LABEL_INGRESS_PATH+g" /workspace/MONAILabel/plugin
 
 exec "$@" &
 
-cd MONAILabel && BUILD_OHIF=true pip install -e .
+if [[ -n "${ENABLE_MONAI_LABEL}" ]]; then
+  cd MONAILabel && BUILD_OHIF=true pip install -e .
 
-pip install dicomweb-client[gcp]
-pip install opencv-python==3.4.18.65 mlflow
+  pip install dicomweb-client[gcp]
+  pip install opencv-python==3.4.18.65 mlflow
 
-
-
-if [[ -z "${BUNDLE_MODEL_NAME}" ]]; then
-  monailabel apps --name radiology --download --output .
-  MONAI_LABEL_API_STR=$MONAI_LABEL_INGRESS_PATH monailabel start_server --app radiology --studies $ORTHANC_DICOMWEB_ADDRESS --conf models deepedit &
-
-else
-  if [[ -z "${NIFTI_FOLDER}" ]]; then
-    monailabel apps --name monaibundle --download --output .
-    python /workspace/mlflow_model_download.py
-    MONAI_LABEL_API_STR=$MONAI_LABEL_INGRESS_PATH monailabel start_server --app monaibundle --studies $ORTHANC_DICOMWEB_ADDRESS --conf models $BUNDLE_MODEL_NAME &
+  if [[ -z "${BUNDLE_MODEL_NAME}" ]]; then
+    monailabel apps --name radiology --download --output .
+    MONAI_LABEL_API_STR=$MONAI_LABEL_INGRESS_PATH monailabel start_server --app radiology --studies $ORTHANC_DICOMWEB_ADDRESS --conf models deepedit &
 
   else
-    monailabel apps --name monaibundle --download --output .
-    python /workspace/mlflow_model_download.py
-    MONAI_LABEL_API_STR=$MONAI_LABEL_INGRESS_PATH monailabel start_server --app monaibundle --studies $NIFTI_FOLDER --conf models $BUNDLE_MODEL_NAME &
+    if [[ -z "${NIFTI_FOLDER}" ]]; then
+      monailabel apps --name monaibundle --download --output .
+      python /workspace/mlflow_model_download.py
+      MONAI_LABEL_API_STR=$MONAI_LABEL_INGRESS_PATH monailabel start_server --app monaibundle --studies $ORTHANC_DICOMWEB_ADDRESS --conf models $BUNDLE_MODEL_NAME &
 
+    else
+      monailabel apps --name monaibundle --download --output .
+      python /workspace/mlflow_model_download.py
+      MONAI_LABEL_API_STR=$MONAI_LABEL_INGRESS_PATH monailabel start_server --app monaibundle --studies $NIFTI_FOLDER --conf models $BUNDLE_MODEL_NAME &
+
+    fi
   fi
 fi
 
