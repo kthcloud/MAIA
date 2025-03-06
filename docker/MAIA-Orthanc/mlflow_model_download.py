9b855170a76b34bda14d0349f0e6369addc6ce4e
simben <simben@kth.se>
2025-03-06 11:16:07 +0100

    Add MAIA-Orthanc Docker setup with environment configuration, scripts, and Helm chart updates

--- docker/MAIA-Orthanc/mlflow_model_download.py
+++ docker/MAIA-Orthanc/mlflow_model_download.py
@@ -1,25 +1,24 @@
 import os
 import shutil
-import mlflow
 from pathlib import Path
 import zipfile
 import subprocess
 
 
 #model_uri = os.environ["MLFLOW_MODEL"]
-dst_path = os.environ["MLFLOW_MODEL_PATH"]
-monai_bundle_path = os.environ["MONAI_BUNDLE_PATH"]
-
 #Path(dst_path).mkdir(parents=True,exist_ok=True)
-Path(monai_bundle_path).mkdir(parents=True,exist_ok=True)
 #try:
 #    mlflow.pytorch.load_model(f"models:/{model_uri}/latest", dst_path=dst_path)
 #except:
 #    ...
+dst_path = os.environ["MLFLOW_MODEL_PATH"]
+monai_bundle_path = os.environ["MONAI_BUNDLE_PATH"]
 
-for file in os.listdir(str(Path(dst_path).joinpath("extra_files"))):
+Path(monai_bundle_path).mkdir(parents=True,exist_ok=True)
+
+for file in os.listdir(dst_path):
     if file.endswith(".zip"):
-        shutil.copy(Path(dst_path).joinpath(dst_path,"extra_files",file), monai_bundle_path)
+        shutil.copy(Path(dst_path).joinpath(file), monai_bundle_path)
 
         with zipfile.ZipFile(Path(monai_bundle_path).joinpath(file), 'r') as zip_ref:
             zip_ref.extractall(monai_bundle_path)
