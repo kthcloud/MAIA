e9a225f18b595370defd474e49c513cf88ad4edd
simben <simben@kth.se>
2025-03-15 19:54:15 +0100

    Cleanup start.sh by removing unused installation scripts and related commands

--- docker/Notebooks/Base/start.sh
+++ docker/Notebooks/Base/start.sh
@@ -12,26 +12,8 @@ until [ -d "$HOME/Desktop" ]; do
   sleep 1
 done
 
-bash /etc/change_desktop_wallpaper.sh
-
 if [ "$INSTALL_ZSH" = "1" ]; then
     /etc/install_zsh.sh
-fi
-
-if [ "$INSTALL_SLICER" = "1" ]; then
-    /etc/install_slicer.sh
-fi
-
-if [ "$INSTALL_FREESURFER" = "1" ]; then
-    /etc/install_freesurfer.sh
-fi
-
-if [ "$INSTALL_ITKSNAP" = "1" ]; then
-    /etc/install_itksnap.sh
-fi
-
-if [ "$INSTALL_QUPATH" = "1" ]; then
-    /etc/install_qupath.sh
 fi
 
 if [ ! -f "$HOME/.zshrc" ]; then
