211cd62b72fec66f5a6b5985dba7558ebc5e107c
simben <simben@kth.se>
2025-03-21 11:09:14 +0100

    Refactor GPU booking verification to use dot notation for accessing attributes

--- MAIA/dashboard_utils.py
+++ MAIA/dashboard_utils.py
@@ -63,9 +63,9 @@ def verify_gpu_availability(global_existing_bookings, new_booking, gpu_specs):
 
     overlapping_allocations = []
     for existing_booking in global_existing_bookings:
-        if existing_booking["gpu"] == gpu_name:
-            existing_booking_start = datetime.strptime(existing_booking["start_date"], "%Y-%m-%d %H:%M:%S")
-            existing_booking_end = datetime.strptime(existing_booking["end_date"], "%Y-%m-%d %H:%M:%S")
+        if existing_booking.gpu == gpu_name:
+            existing_booking_start = datetime.strptime(existing_booking.start_date, "%Y-%m-%d %H:%M:%S")
+            existing_booking_end = datetime.strptime(existing_booking.end_date, "%Y-%m-%d %H:%M:%S")
             new_booking_start = datetime.strptime(new_booking["start_date"], "%Y-%m-%d %H:%M:%S")
             new_booking_end = datetime.strptime(new_booking["end_date"], "%Y-%m-%d %H:%M:%S")
 
@@ -101,8 +101,8 @@ def verify_gpu_availability(global_existing_bookings, new_booking, gpu_specs):
         available_gpus = gpu_replicas * gpu_count
         gpu_availability_per_slot.append(available_gpus)
         for existing_booking in global_existing_bookings:
-            existing_booking_start = datetime.strptime(existing_booking["start_date"], "%Y-%m-%d %H:%M:%S")
-            existing_booking_end = datetime.strptime(existing_booking["end_date"], "%Y-%m-%d %H:%M:%S")
+            existing_booking_start = datetime.strptime(existing_booking.start_date, "%Y-%m-%d %H:%M:%S")
+            existing_booking_end = datetime.strptime(existing_booking.end_date, "%Y-%m-%d %H:%M:%S")
 
             if existing_booking_start < overlapping_window_end and existing_booking_end > overlapping_window_start:
                 available_gpus -= 1
