1eb09168cc365c418e56f86466b1f20e8baee4ba
simben <simben@kth.se>
2025-05-10 13:45:27 +0200

    Refactor code structure for improved readability and maintainability

--- docker/MAIA-Workspace/Tutorials/Slicer_Extensions.py
+++ docker/MAIA-Workspace/Tutorials/Slicer_Extensions.py
@@ -15,7 +15,10 @@ if __name__ == "__main__":
         "DCMQI",
         "PETDICOMExtension",
         "SegmentEditorExtraEffects",
+        "SlicerConda",
         #"SlicerDevelopmentToolbox",
+        #"DatabaseInteractor",
+        #"DebuggingTools",
         #"MarkupsToModel",
         #"Chest_Imaging_Platform",
         #"MONAIViz",
@@ -34,7 +37,11 @@ if __name__ == "__main__":
         "SegmentWithSAM",
         "SlicerDcm2nii",
         "TCIABrowser",
-        "nnUNet"
+        "nnUNet",
+        "NNInteractive",
+        "ImageAugmenter",
+        "XNATSlicer",
+        "SlicerFreeSurfer",
     ]
 
     load_extensions(extensions)
